a00c430abb11aacc5122a0d2239462d7071cc531
muta <muttaroni8827@gmail.com>
2021-04-21 20:28:08 +0900

    feat: http handling in crud_application

---
+++ crud_application/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"encoding/json"
+	"net/http"
+)
+
+// reference: https://qiita.com/Syoitu/items/8e7e3215fb7ac9dabc3a
+func main() {
+	mux := http.NewServeMux()
+    mux.HandleFunc("/", func(w http.ResponseWriter, q *http.Request) {
+        message := map[string]string{
+            "message": "hello world",
+        }
+        jsonMessage, err := json.Marshal(message)
+        if err != nil {
+            panic(err.Error())
+        }
+        w.Write(jsonMessage)
+    })
+	http.ListenAndServe("127.0.0.1:8080", mux)
+	fmt.Println("Hello World!")
+}
